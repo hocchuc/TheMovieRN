6a38889474c370024bccd538dcc1f5f88faa69f2
Howard <howard@vinova.sg>
2018-12-26 19:06:18 +0700

    * adjust layout

--- app/components/MovieItem/index.js
+++ app/components/MovieItem/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { View, Text, StyleSheet, TouchableOpacity, Image } from 'react-native';
 import {IMAGE_URL, POSTER_SIZE} from '../../constant/Constants';
+
 export default class MovieItem extends Component {
   constructor(props) {
     super(props);
@@ -12,13 +13,11 @@ export default class MovieItem extends Component {
     return (
         <TouchableOpacity onPress={this.onPress} >
         <View style = {style.container}>
-            <Image source={{uri: image}}
-            style={{width: 100, height: 185}}  />
-            <View style = {[
-                style.textContainer, 
-                {  backgroundColor: this.props.index % 2 == 0 ? '#f73378' : 'gray'}  ]}>
+            <Image source={{uri: image}} style={style.imageContainer}  />
+            <View style = {[style.textContainer, 
+                { backgroundColor: this.props.index % 2 == 0 ? '#f73378' : 'gray'} ]}>
                 <Text style ={style.title}>{this.props.item.title}</Text>
-                <Text style ={style.content}>{this.props.item.overview}</Text>
+                <Text style ={style.content} ellipsizeMode={'tail'} numberOfLines={10}>{this.props.item.overview}</Text>
 
             </View>
         </View>
@@ -28,6 +27,10 @@ export default class MovieItem extends Component {
 }
 
 const style = StyleSheet.create({
+    imageContainer: {
+        width: 100,
+        height: 185
+    },
     container: {
         flex: 1,
         flexDirection: 'row',
@@ -38,6 +41,7 @@ const style = StyleSheet.create({
         flex: 1,
         flexDirection: 'column',
         marginLeft: 8,
+        height: 185
     },
     title: {
         alignContent: 'flex-start',
@@ -46,8 +50,11 @@ const style = StyleSheet.create({
         backgroundColor:'yellow'
     },
     content: {
+        flex: 1,
+        flexWrap: 'wrap',
         color: 'white',
         backgroundColor:'blue',
-        padding: 4,
+        padding: 15,
+        justifyContent: 'space-between',
     }
 })
