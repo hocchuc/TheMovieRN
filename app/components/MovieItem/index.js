fdb820e17287cce8c2237047bf0180f2b4a7f781
Howard <howard@vinova.sg>
2019-01-17 00:27:11 +0700

    * handle onClick, edit item UI)

--- app/components/MovieItem/index.js
+++ app/components/MovieItem/index.js
@@ -1,24 +1,32 @@
 import React, { Component } from 'react';
 import { View, Text, StyleSheet, TouchableOpacity, Image } from 'react-native';
-import {IMAGE_URL, POSTER_SIZE} from '../../constant/Constants';
+import {IMAGE_URL, POSTER_SIZE_MEDIUM_RES} from '../../constant/Constants';
 
 export default class MovieItem extends Component {
   constructor(props) {
     super(props);
   }
+  _onPress = () => {
+      console.log(this.props)
+      console.log("onClick inside item")
+      this.props.onPress()
+    // your code on item press
+ };
 
   render() {
-    const image = IMAGE_URL + POSTER_SIZE +"/"+ this.props.item.poster_path;
-    console.log(image)
+    const image = IMAGE_URL + POSTER_SIZE_MEDIUM_RES +"/"+ this.props.item.poster_path;
+
+    //console.log(image)
     return (
-        <TouchableOpacity onPress={this.onPress} >
+        <TouchableOpacity onPress={this._onPress}>
         <View style = {style.container}>
             <Image source={{uri: image}} style={style.imageContainer}  />
-            <View style = {[style.textContainer, 
-                { backgroundColor: this.props.index % 2 == 0 ? '#f73378' : 'gray'} ]}>
+            <View style = {[style.textContainer]}>
                 <Text style ={style.title}>{this.props.item.title}</Text>
-                <Text style ={style.content} ellipsizeMode={'tail'} numberOfLines={10}>{this.props.item.overview}</Text>
-
+                <Text style ={style.content} 
+                      ellipsizeMode={'tail'} 
+                      numberOfLines={10}>
+                      {this.props.item.overview}</Text>
             </View>
         </View>
     </TouchableOpacity>
@@ -29,7 +37,9 @@ export default class MovieItem extends Component {
 const style = StyleSheet.create({
     imageContainer: {
         width: 100,
-        height: 185
+        height: 185,
+        backgroundColor: 'blue',
+        marginRight: 8
     },
     container: {
         flex: 1,
@@ -40,14 +50,14 @@ const style = StyleSheet.create({
     textContainer: {
         flex: 1,
         flexDirection: 'column',
-        marginLeft: 8,
-        height: 185
+        backgroundColor:'black', 
+        height: 185,
     },
     title: {
         alignContent: 'flex-start',
-        color: 'orange',
         fontSize: 16,
-        backgroundColor:'yellow'
+        paddingLeft: 16,
+        color: 'white'
     },
     content: {
         flex: 1,
--- app/screens/MovieList/index.js
+++ app/screens/MovieList/index.js
@@ -1,26 +1,30 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, FlatList} from 'react-native';
+import { View, FlatList, Image} from 'react-native';
 import {URL} from '../../constant/Constants';
 import MovieItem from '../../components/MovieItem';
-
+import {styles} from './style';
 export class MovieList extends Component {
   constructor(props) {
     super(props);
     this.state = {
+     // isLoading = true
     };
   }
   
+  callBackFromItem = () => {
+    console.log("callback from item")
+  }
+
   async componentDidMount() {    
       console.log("1")
     return fetch(URL)
     .then((response) => response.json())
     .then((responseJson) => {
-        console.log(JSON.stringify(responseJson))
+        console.log(JSON.stringify(responseJson)) //just for testing
         this.setState({
             isLoading  : false,
             dataSource : responseJson.results,
         }, function(){
-
         })
     })
     .catch((error) => {
@@ -31,47 +35,19 @@ export class MovieList extends Component {
   
   render() {
     const {navigate} = this.props.navigation;
-    return (
+      return (
         <View style={styles.container}>
         <FlatList
           data= {this.state.dataSource}
           renderItem={({item, index}) =>
            <MovieItem
            item = {item}
-           index = {index}/>
+           index = {index}
+           onPress={this.callBackFromItem}/>
           }
           keyExtractor={({id}, index) => id}
         />
       </View>
     );
-  }
-
-  
+  } 
 }
-
-async function callMoviesAPI() {
-    // console.log(URL)
-    // try {
-    //     const response = await fetch(URL);
-    //     const responseJson = await response.json();  
-    //     this.setState({
-    //         dataList: responseJson.results,
-    //     });
-    //   } catch (error) {
-    //     console.error(error);
-    // }
-  }
-
-const styles = StyleSheet.create({
-    container: {
-     flex: 1,
-     padding: 22
-    },
-    item: {
-      padding: 10,
-      fontSize: 18,
-      height: 44,
-    },
-  })
-  
-  
